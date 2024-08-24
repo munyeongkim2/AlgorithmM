a140ebbfc758120085cfeb22fd9c38058d1275b9
김문영 <115964167+munyeongkim2@users.noreply.github.com>
2024-08-24 15:55:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 305.24 ms, Memory: 94.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,23 +2,14 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
     
-        List<String>[] list = new List[100];
-
-        for(int i = 0; i < list.length; i++){
-            list[i] = new ArrayList<>();
-        }
-
-        for (String s : completion) {
-            list[Math.abs(s.hashCode()) % list.length].add(s);
-        }
-
-        for (String s : participant) {
-            if(!list[Math.abs(s.hashCode()) % list.length].remove(s)){
-                return s;
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        for(int i = 0; i < completion.length ; i ++){
+            if(!participant[i].equals(completion[i])){
+                return participant[i];
             }
         }
-         
-        return null;
+        return participant[participant.length - 1];
     }
     
     
